eb2022611a613661796a52165bc6f5f448199fcf
LiJingZhi <li.jingzhi@iwhalecloud.com>
2024-03-25 10:12:54 +0800

    commit

--- src/main/java/com/example/mybatis/model/User.java
+++ src/main/java/com/example/mybatis/model/User.java
@@ -15,6 +15,10 @@ public class User {
     private Integer id;
     private String name;
 
+    private String startDate;
+
+    private String endDate;
+
     public User(Integer id, String name) {
         this.id = id;
         this.name = name;
@@ -25,5 +29,37 @@ public class User {
 
     public User(Integer id) {
         this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
     }
 }
